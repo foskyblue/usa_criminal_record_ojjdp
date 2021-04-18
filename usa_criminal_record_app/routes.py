16759c99486af2f35c4c891b9088394d76c1c068
Ufuoma Ubor <foskynew@gmail.com>
2021-04-18 12:46:41 +0100

    feat: routing chart figures to the front-end

--- usa_criminal_record_app/routes.py
+++ usa_criminal_record_app/routes.py
@@ -1,13 +1,30 @@
 import json
 import plotly
-from flask import render_template
-from data_wrangling_scripts.data_wrangling import return_figures
+from flask import render_template, request, url_for
+from data_wrangling_scripts.data_wrangling import get_figures
 
 from usa_criminal_record_app import app
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['POST'])
 def index():
 
-    return render_template('index.html')
+    year = 2019
+    offense = 'All offenses'
+
+    # get form values
+    if request.method == 'POST':
+        year = request.form.get('year')
+        offense = request.form.get('offense')
+
+    # get figures
+    figures = get_figures(year, offense)
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html', ids=ids, figuresJSON=figuresJSON)
